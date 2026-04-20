7cf68bb4fcef6fa1440f4a4c7182cbbfdae80905
Egwaldino Cassuente <egcassuente@gmail.com>
2026-04-20 20:36:55 +0100

    Completed style ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,139 @@
-const Home = () => { 
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import {
+  Check,
+  CircleSlash,
+  List,
+  ListCheck,
+  Plus,
+  Sigma,
+  SquarePen,
+  Trash,
+} from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
-      <p className="mt-4 text-lg text-gray-600">This is the home page.</p>
-    </div>
+    <main className="p-4 w-full bg-gray-100 h-screen flex justify-center items-center">
+      <Card className="w-lg">
+        <CardHeader className="flex p-2 gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button className="cursor-pointer" variant="default">
+            <Plus />
+            Cadastrar
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4" />
+
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer py-3" variant="default">
+              <List />
+              Todas
+            </Badge>
+            <Badge className="cursor-pointer py-3" variant="outline">
+              <CircleSlash />
+              Não finalizadas
+            </Badge>
+            <Badge className="cursor-pointer py-3" variant="outline">
+              <Check />
+              Finalizadas
+            </Badge>
+          </div>
+
+          <div className="mt-4 border-b-[1]">
+            <div className="h-12 flex justify-between items-center border-t-[1] ">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar React</p>
+              <div className="flex items-center gap-2">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar Tarefa</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar tarefa" />
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListCheck size={16} />
+              <p className="text-xs">Tarefas concluídas: (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="cursor-pointer text-xs h-7"
+                  variant="outline"
+                >
+                  <Trash />
+                  Limpar tarefas concluídas
+                </Button>
+              </AlertDialogTrigger>
+
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir x itens?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Não</AlertDialogCancel>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-200 mt-4 rounded-md">
+            <div
+              className="h-full bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tarefas no total</p>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   );
-}
+};
 
 export default Home;
